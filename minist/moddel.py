edc81ac86b223637a42b445cf03d0f99cd37dbce
su <673399718@qq.com>
2018-06-14 23:59:24 +0800

    .

--- minist/moddel.py
+++ minist/moddel.py
@@ -10,7 +10,7 @@ cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 init = tf.initialize_all_variables()
 sess = tf.Session()
-sess.run(init)
+sess.run(init)TimeoutErrorX
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
